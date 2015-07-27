89542435a3098ca7c8ddc9a1b15e5a6d27c164bc
TuanDN2 <kingdragon102@gmail.com>
2015-07-27 11:09:46 +0700

    Video Record Camera(NF)

--- app/src/main/java/tuandn/com/mediatraining/Fragments/VideoRecordFragment.java
+++ app/src/main/java/tuandn/com/mediatraining/Fragments/VideoRecordFragment.java
@@ -2,19 +2,26 @@ package tuandn.com.mediatraining.Fragments;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraManager;
-import android.net.Uri;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import java.io.File;
+import java.io.IOException;
+
+import tuandn.com.mediatraining.Activity.MainActivity;
 import tuandn.com.mediatraining.R;
 
 /**
@@ -27,6 +34,15 @@ public class VideoRecordFragment extends Fragment{
     private Activity mActivity;
     private Context mContext;
     private VideoView mVideoView;
+
+    private String filenameToSaveDB;
+    private String targetFilename;
+    private Button videoRecord;
+    private MediaRecorder recorder;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,33 +58,75 @@ public class VideoRecordFragment extends Fragment{
         mActivity = getActivity();
         mContext = getActivity().getApplicationContext();
         mVideoView = (VideoView) getView().findViewById(R.id.video_view);
+        videoRecord = (Button) getView().findViewById(R.id.video_record);
+        surfaceView = (SurfaceView) getView().findViewById(R.id.surface);
+        surfaceHolder = surfaceView.getHolder();
 
         //Setting for Floating Button
         fab = (FloatingActionButton) getView().findViewById (R.id.video_floating_button);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakeVideoIntent();
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    useCameraAPI();
+                } else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    useCameraAPI();
+                }
+            }
+        });
+
+        videoRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recorder.stop();
+                recorder.reset();
+                recorder.release();
+
+                camera.stopPreview();
+                camera.release();
+                Toast.makeText(mContext,"Stopped",Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private void dispatchTakeVideoIntent() {
-        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        if (takeVideoIntent.resolveActivity(mActivity.getPackageManager()) != null) {
-            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+
+
+
+    private void useCameraAPI(){
+        camera = Camera.open();
+        // If necessary, modify the returned Camera.Parameters object and call setParameters(Camera.Parameters).
+        camera.getParameters();
+        // Set Camera orientation
+        camera.setDisplayOrientation(90);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.stopPreview();
+            camera.unlock();
+
+            recorder = new MediaRecorder();
+            recorder.setCamera(camera);
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+            filenameToSaveDB = "Video_"
+                    + System.currentTimeMillis()
+                    + ".mp4";
+            targetFilename = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator
+                    +filenameToSaveDB;
+            recorder.setOutputFile(targetFilename);
+            recorder.prepare();
+            recorder.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == mActivity.RESULT_OK) {
-            Uri videoUri = data.getData();
-            mVideoView.setVideoURI(videoUri);
-        }
-    }
-
-    private void videoRecord(){
+    private void useCamera2API(){
 
     }
 }
